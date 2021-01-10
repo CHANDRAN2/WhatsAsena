38ddc66354729321ecf260bb89e42f367b8d1f1e
CHANDRAN2 <73172693+CHANDRAN2@users.noreply.github.com>
2021-01-11 01:47:40 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -192,15 +192,17 @@ Asena.addCommand({pattern: 'img ?(.*)', fromMe: false, desc: Lang.IMG_DESC}, (as
     gis(match[1], async (error, result) => {
         for (var i = 0; i < (result.length < 3 ? result.length : 3); i++) {
             var get = got(result[i].url, {https: {rejectUnauthorized: false}});
-            if(error){
-                await message.client.sendMessage(message.jid,'```Error Fetching Images!```', MessageType.text);  
-            }
-            if(!error) {
-                var stream = get.buffer();
-                    
-                stream.then(async (image) => {
-                    await message.client.sendMessage(message.jid,image, MessageType.image);
-                });
+            async function(err) {
+                if(err){
+                    await message.client.sendMessage(message.jid,'```Error Fetching Images!```', MessageType.text);  
+                }
+                if(!err) {
+                    var stream = get.buffer();
+
+                    stream.then(async (image) => {
+                        await message.client.sendMessage(message.jid,image, MessageType.image);
+                    });
+                }
             }
         }
         message.reply(Lang.IMG.format((result.length < 3 ? result.length : 3), match[1]));
