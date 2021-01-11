02a78188b2be7ca6e3abbcef5937ad4a47390c92
CHANDRAN2 <73172693+CHANDRAN2@users.noreply.github.com>
2021-01-11 17:28:20 +0530

    Update stickers.js

--- plugins/stickers.js
+++ plugins/stickers.js
@@ -18,7 +18,7 @@ const Lang = Language.getString('sticker');
 
 Asena.addCommand({pattern: 'sticker', fromMe: false, desc: Lang.STICKER_DESC}, (async (message, match) => {    
     if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);
-    if (message.reply_message.text === true) return await message.sendMessage('```Reply to a Image, GIF or a Short Video```');
+    //if (message.reply_message.text === true) return await message.sendMessage('```Reply to a Image, GIF or a Short Video```');
     if (message.reply_message.text === false){
         var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
         var location = await message.client.downloadAndSaveMediaMessage({
