f74f5ecf512f7bc8ad439129ca66b9002f330f4a
CHANDRAN2 <73172693+CHANDRAN2@users.noreply.github.com>
2021-01-11 17:52:39 +0530

    Update stickers.js

--- plugins/stickers.js
+++ plugins/stickers.js
@@ -1,8 +1,6 @@
 /* Copyright (C) 2020 Yusuf Usta.
-
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
 WhatsAsena - Yusuf Usta
 */
 
@@ -18,29 +16,26 @@ const Lang = Language.getString('sticker');
 
 Asena.addCommand({pattern: 'sticker', fromMe: false, desc: Lang.STICKER_DESC}, (async (message, match) => {    
     if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);
-    //if (message.reply_message.text === true) return await message.sendMessage('```Reply to a Image, GIF or a Short Video```');
-    if (message.reply_message.text === false){
-        var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
-        var location = await message.client.downloadAndSaveMediaMessage({
-            key: {
-                remoteJid: message.reply_message.jid,
-                id: message.reply_message.id
-            },
-            message: message.reply_message.data.quotedMessage
+    var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
+    var location = await message.client.downloadAndSaveMediaMessage({
+        key: {
+            remoteJid: message.reply_message.jid,
+            id: message.reply_message.id
+        },
+        message: message.reply_message.data.quotedMessage
+    });
+
+    if (message.reply_message.video === false && message.reply_message.image) {
+        execFile(cwebp, [location, '-o', 'output.webp'], async err => {
+            if (err) {
+                throw err;
+            }
+        
+            await message.sendMessage(fs.readFileSync('./output.webp'), MessageType.sticker);
         });
-
-        if (message.reply_message.video === false && message.reply_message.image) {
-            execFile(cwebp, [location, '-o', 'output.webp'], async err => {
-                if (err) {
-                    //return await message.sendMessage('```Reply to a Image, GIF or a Short Video```');
-                    throw err
-                }
-
-                await message.sendMessage(fs.readFileSync('./output.webp'), MessageType.sticker);
-            });
-            return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
-        }
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }
+
     ffmpeg(location)
         .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-loop 0", "-an", "-vsync 0", "-s 512x512"])
         .save('sticker.webp')
