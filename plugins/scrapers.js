4cd3dc8a94378eddb0d1df96da0c70f6c526acd0
CHANDRAN2 <73172693+CHANDRAN2@users.noreply.github.com>
2021-01-11 22:27:02 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -187,15 +187,15 @@ Asena.addCommand({pattern: 'wiki ?(.*)', fromMe: false, desc: Lang.WIKI_DESC}, (
     await reply.delete();
 }));
 
-Asena.addCommand({pattern: 'img ?(.*)', fromMe: false, desc: Lang.IMG_DESC}, (async (message, match) => { 
+Asena.addCommand({pattern: 'img ?(.*)', fromMe: true, desc: Lang.IMG_DESC}, (async (message, match) => { 
     if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);
     gis(match[1], async (error, result) => {
-        for (var i = 0; i < (result.length < 3 ? result.length : 3); i++) {
+        for (var i = 0; i < (result.length < 5 ? result.length : 5); i++) {
             var get = got(result[i].url, {https: {rejectUnauthorized: false}});
             var stream = get.buffer();
                 
             stream.then(async (image) => {
-                try{
+                try {
                     await message.client.sendMessage(message.jid,image, MessageType.image);
                 } catch {
                     await message.client.sendMessage(message.jid,'```Error Fetching Images!```', MessageType.text);
@@ -203,6 +203,6 @@ Asena.addCommand({pattern: 'img ?(.*)', fromMe: false, desc: Lang.IMG_DESC}, (as
             });
         }
 
-        message.reply(Lang.IMG.format((result.length < 3 ? result.length : 3), match[1]));
+        message.reply(Lang.IMG.format((result.length < 5 ? result.length : 5), match[1]));
     });
 }));
