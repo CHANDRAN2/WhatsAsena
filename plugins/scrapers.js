9b7b2b96cac3eba4964548d633e9eb6776ad6655
CHANDRAN2 <73172693+CHANDRAN2@users.noreply.github.com>
2021-01-12 10:21:46 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -190,7 +190,7 @@ Asena.addCommand({pattern: 'wiki ?(.*)', fromMe: false, desc: Lang.WIKI_DESC}, (
 Asena.addCommand({pattern: 'img ?(.*)', fromMe: false, desc: Lang.IMG_DESC}, (async (message, match) => { 
     if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);
     gis(match[1], async (error, result) => {
-        for (var i = 0; i < (result.length < 5 ? result.length : 5); i++) {
+        for (var i = 0; i < (result.length < 3 ? result.length : 3); i++) {
             var get = got(result[i].url, {https: {rejectUnauthorized: false}});
             var stream = get.buffer();
                 
@@ -203,6 +203,6 @@ Asena.addCommand({pattern: 'img ?(.*)', fromMe: false, desc: Lang.IMG_DESC}, (as
             });
         }
 
-        message.reply(Lang.IMG.format((result.length < 5 ? result.length : 5), match[1]));
+        message.reply(Lang.IMG.format((result.length < 3 ? result.length : 3), match[1]));
     });
 }));
